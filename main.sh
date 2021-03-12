6b1e55e5bc7437aad468dc71c54c88388ef3bf0d
ELHAM NUSRAT <elham.nusrat@g.bracu.ac.bd>
2021-03-12 12:06:47 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+echo "(1)"
+a=5
+b="my name"
+
+echo b
+#printing value
+echo $b
+
+#inline
+echo a echo $a
+
+
+
+
+echo "(2)"
+echo "Enter a name:"
+#"read" takes input
+#"name" is a variable
+read naam
+echo "The name of the user is $naam"
+
+
+
+
+echo "(3)"
+#Arithmetic calculations
+x=4
+y=2
+echo $x+$y
+z=$x+$y
+echo $z
+
+w=$(($x+$y))
+echo $w
+
+q=$(($x-$y))
+echo $q
+
+e=$(($x/$y))
+echo $e
+
+r=$(($x*$y))
+echo $r
+
+t=$(($x**$y))
+echo $t
+
+
+
+
+echo "(4)"
+#if-else condition in bash
+echo "Enter a number: "
+read u
+
+echo "Enter another number: "
+read i
+
+if [ $u -gt $i ];
+  then echo "$u is greater than $i"
+elif [ $u -lt $i ];
+  then echo "$u is less than $i"
+else
+  echo "$u and $i are equal"
+fi
+#"fi" indicates the ending of "if"
+
+echo "(5)"
+#example
+echo "Enter a name:"
+read filename
+
+if [ -d $filename ];
+  then echo "It is a directory"
+elif [ -f $filename ];
+  then echo "It is a file"
+else 
+  echo "Not a ffile or directory"
+fi
+
+echo "(6)"
+#switch case
+echo "Enter a name:"
+read name
+
+case $name in 
+  "Abul") echo "The name is Abul";;
+  "Kashem") echo "The name is Kashem";;
+  "Elham") echo "Love from Elham";
+           echo "Goodnight";;
+  *) echo "Something else"
+  # * everything else
+esac
+#"esac" inding of case
+
+
+
+echo "(7)"
+#For-loops
+#traditional for-loop
+for ((i=0; i<10; i++))
+do
+  echo $i
+done
+
+echo "(8)"
+#enhance for-loop, fr-loop in python
+my_list=(h j k l)
+
+for i in ${my_list[*]};
+do
+  echo $i
+done
+
+echo "(9)"
+#while-loop
+i=0
+
+while [ $i -lt 10 ];
+do
+  echo -n $i
+  i=$(($i+1))
+done
+
+echo "(10)"
+#until-loop
+
+
+
+echo "(11)"
+#command-line arguments
+echo "Argument number 1: "$1
+echo "Argument number 0: "$0
+echo "Argument number 4: "$4
+echo "Total arguments: " $#
+echo "All the arguments: "$*
+
+
+
+echo "(12)"
+#Functions in bash
+
+function clock(){
+  echo "tic toc, tic toc"
+}
+
+clock
+
+#or we can right without "function"
+bell(){
+  g="ghost"
+  echo "ting tong, ting tong"
+  echo $g "inside the house" 
+}
+bell
+echo $g "outside the house"
+
+#replaces previous value in same variable 
+g="vampire"
+echo "Hi" $g
+
+#save value only inside the function
+saveInside(){
+  local l="saved in function"
+  echo $l
+}
+saveInside
+echo $l "saved nowhere"
+
+
+#function with parameters
+adder (){
+  echo $(( $1 + $2 ))
+}
+adder 5 6
+
+
+#return a value from function
+
+returnMything(){
+  borrowed=11
+  return $borrowed
+}
+returnMything
+echo $?"$"
